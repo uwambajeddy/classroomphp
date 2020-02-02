f3304b70afd630a707fe548525a9e1f5425e2b3c
mugisha12 <58938904+mugisha12@users.noreply.github.com>
2020-02-02 20:28:30 +0200

    profile update

--- process.php
+++ process.php
@@ -31,3 +31,77 @@ if (isset($_POST['log-submit'])) {
         }
     }
 }
+
+if (isset($_POST['save'])) {
+    $id = $_POST['id'];
+    $username = $_POST['username'];
+    $phone=$_POST['phone'];
+    $email = $_POST['email'];
+    $pass = $_POST['pass'];
+    $cpass = $_POST['c_pass'];
+    $bio = $_POST['bio'];
+    $password = $_POST['password'];
+    $add_image=$_FILES['image']['name'];
+    $imagetmp=$_FILES['image']['tmp_name'];
+    $folder="img/";
+
+    if (empty($username)) {
+        array_push($array, "Username is require");
+    }
+    if (empty($phone)) {
+        array_push($array, "Phone is require");
+    }
+    if (empty($email)) {
+        array_push($array, "Email is require");
+    }
+    if (empty($password)) {
+        array_push($array, "Password is require");
+    }
+    if (count($array) == 0) {
+        if ($pass == $cpass){
+
+            $pass_c = md5($password);
+            $sql = "SELECT * FROM `users` WHERE `id` = '$id' AND `password` ='$pass_c'";
+
+            $query = mysqli_query($con, $sql);
+
+            while ($row=mysqli_fetch_array($query)) { 
+                $e_im=$row['image'];
+                $e_b=$row['bio'];
+            }
+
+            if (empty($add_image)) {
+                    $add_image= $e_im;
+            }
+            if (empty($bio)) {
+                    $bio=$e_b;
+            }
+
+
+            if (mysqli_num_rows($query) > 0) {
+                if(empty($pass) || empty($cpass)){
+                    $npass = md5($password); 
+                }else{
+                    $npass = md5($pass);
+                }
+                
+                move_uploaded_file($imagetmp,$folder.$add_image);
+                $sql="UPDATE `users` SET `username`='$username',`password`='$npass',`email`='$email',`phone`='$phone',`image`='$add_image',`bio`='$bio' WHERE `id`=$id";
+                $query=mysqli_query($con, $sql);
+                if ($query) {
+                    header('location: profile.php');
+                }else{
+                    array_push($array, "Update failed");
+                }
+
+            }else{
+                array_push($array, "Incorrect Password");
+            }
+
+        }else{
+            array_push($array, "Your password are not equal");
+        }
+        
+
+    }
+}
--- profile.php
+++ profile.php
@@ -31,27 +31,43 @@ if (!isset($_SESSION['username'])) {
         <div class="flex-3"></div>
         <div class="flex-1">
             <h2>User Form</h2>
-            <form class="form-style-9" method="post" action="server.php" enctype="multipart/form-data">
+            <?php
+
+                    if (count($array) > 0) {
+                        for ($a = 0; $a <= sizeof($array) - 1; $a++) { ?>
+                            <div class="m-danger">
+                                <?php echo $array[$a]; ?>
+                            </div>
+                    <?php   }
+                    } ?>
+            <?php 
+                $id_info=$_SESSION['username'];
+                $sql="SELECT * FROM `users` WHERE `username`='$id_info'";
+                $query=mysqli_query($con, $sql);
+                while ($row=mysqli_fetch_array($query)) 
+                { ?>
+            <form class="form-style-9" method="post" action="profile.php" enctype="multipart/form-data">
                 <ul>
+                   <input type="hidden" name="id" value="<?php echo $row['id']?>">
                     <li>
-                        <input type="text" name="username" class="field-style field-split align-left" placeholder="UserName" />
-                        <input type="email" name="phone" class="field-style field-split align-right" placeholder="Phone number" />
+                        <input type="text" name="username" class="field-style field-split align-left" value="<?php echo $row['username']?>" />
+                        <input type="text" name="phone" class="field-style field-split align-right" value="<?php echo $row['phone']?>" />
 
                     </li>
                     <li>
-                        <input type="text" name="phone" class="field-style field-split align-left" placeholder="enter old password" />
-                        <input type="url" name="field4" class="field-style field-split align-right" placeholder="confirm password" />
+                        <input type="password" name="pass" class="field-style field-split align-left" placeholder="Enter password" />
+                        <input type="password" name="c_pass" class="field-style field-split align-right" placeholder="Confirm password" />
                     </li>
                     <li>
-                        <input type="email" name="email" class="field-style field-full align-none" placeholder="email" />
+                        <input type="email" name="email" class="field-style field-full align-none" value="<?php echo $row['email']?>" />
                     </li>
                     <li>
                         <label for="profileImage">profile image</label>
-                        <input type="file" name="profileImage" class="field-style field-full align-none" />
+                        <input type="file" name="image" class="field-style field-full align-none" />
                     </li>
 
                     <li>
-                        <textarea name="bio" class="field-style" placeholder="your Biography"></textarea>
+                        <textarea name="bio" class="field-style" placeholder="your Biography" name="bio"></textarea>
                     </li>
                     <li>
                         <input type="password" name="password" class="field-style field-full align-none" style="border-color: green " placeholder="enter the old password to confirm changes" />
@@ -62,18 +78,25 @@ if (!isset($_SESSION['username'])) {
                     </li>
                 </ul>
             </form>
+            <?php }?>
         </div>
         <div class="flex-2">
             <h2>User Profile</h2>
+            <?php 
+                $sql="SELECT * FROM `users` WHERE `username`='$id_info'";
+                $query=mysqli_query($con, $sql);
+                while ($row=mysqli_fetch_array($query)) 
+                { ?>
             <div class="form-style-9">
                 <div class="prof-img">
-                    <img src="img/" alt="example">
+                    <img src="img/<?php echo $row['image']?>" alt="example">
                 </div>
                 <div class="field-style">
-                    <textarea name="" id="" cols="30" rows="10">njudsghuiequgeug</textarea>
+                    <h4>Your Biography:</h4>
+                    <p><?php echo $row['bio']?></p>
                 </div>
             </div>
-
+                <?php  } ?>
         </div>
 
     </div>
